24794f907ec0895429eac1cd6844737382943a11
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-10-19 18:27:07 -0300

    interaccion accordion main

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -2,20 +2,23 @@
 
 import { Navbar } from "flowbite-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <Navbar container="true" className="bg-gradient-to-b from-maroon-dark to-blue-dark md:from-blue-dark md:to-maroon-dark">
-      <Navbar.Brand  href="https://flowbite-react.com">
+      
+        <Link href="/">
         <Image
           src="https://eventos.tuentrada.com/light_custom/lightTheme/logo_tue_secutix.png"
           alt="TuEntrada"
           width={197}
           height={89}
           priority
-        />
+          />
+          </Link>
         
-      </Navbar.Brand>
+      
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Navbar.Link href="#" className="text-[1rem] text-white hover:bg-transparent border-b-0 hover:text-slate-400 md:hover:text-slate-400">
