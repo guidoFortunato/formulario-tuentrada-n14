8debe6fc4e16d0ff4a2fcb4980ca3c9d9d32cb9d
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-10-23 13:20:15 -0300

    modificación padding laterales sub categoría

--- src/components/main/AccordionSubCat.jsx
+++ src/components/main/AccordionSubCat.jsx
@@ -19,7 +19,7 @@ const AccordionSubCat = () => {
       <h2 id="accordion-flush-heading-1">
         <button
           type="button"
-          className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 "
+          className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200"
           data-accordion-target="#accordion-flush-body-1"
           aria-expanded="true"
           aria-controls="accordion-flush-body-1"
--- src/components/main/SubCategoria.jsx
+++ src/components/main/SubCategoria.jsx
@@ -4,7 +4,7 @@ import AccordionSubCat from './AccordionSubCat'
 const SubCategoria = () => {
   return (
     
-    <div className="container mx-auto bg-main-image bg-no-repeat bg-left-50 pb-10"> 
+    <div className="container mx-auto bg-main-image bg-no-repeat bg-left-50 pb-10 px-10 md:px-20"> 
     <AccordionSubCat/>
      </div>
   )
