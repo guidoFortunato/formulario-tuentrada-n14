11c08cbd992e95834b3bb6ffba6817f51dfe77c2
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-10-24 17:57:00 -0300

    api, cambios form

--- src/components/footer/Accordion.jsx
+++ src/components/footer/Accordion.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { useState } from "react";
 import { animateScroll as scroll } from "react-scroll";
 
@@ -30,117 +29,117 @@ const Accordion = () => {
       <div
         className={`accordion-content ${
           open ? "max-h-screen" : "max-h-0"
-        } transition-max-h ease-in-out duration-500 overflow-hidden w-full border-t border-gray-200`}
+        } transition-max-h ease-in-out duration-500 overflow-hidden w-full ${open && "border-t border-gray-200"}`}
       >
         <div
-        className={`accordion-content ${
-          open ? "max-h-screen" : "max-h-0"
-        } transition-max-h ease-in-out duration-500 overflow-hidden w-full border-t border-gray-200`}
-      >
-        <div className="py-5 border-gray-200 flex justify-evenly flex-col md:flex-row text-gray-200 mb-2 text-lg font-semibold">
-          <div className="text-center mt-5 md:mt-0">
-            <p>Categorías</p>
-            <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
-              <li>
-                <a href="#" target="_new">
-                  Conciertos
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Exposiciones
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Familia
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Teatro
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Deportes
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Cine
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="text-center mt-5 md:mt-0">
-            <p>La empresa</p>
-            <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
-              <li>
-                <a href="#" target="_new">
-                  Quienes somos
-                </a>
-              </li>
+          className={`accordion-content ${
+            open ? "max-h-screen" : "max-h-0"
+          } transition-max-h ease-in-out duration-500 overflow-hidden w-full border-t border-gray-200`}
+        >
+          <div className="py-5 border-gray-200 flex justify-evenly flex-col md:flex-row text-gray-200 mb-2 text-lg font-semibold">
+            <div className="text-center mt-5 md:mt-0">
+              <p>Categorías</p>
+              <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
+                <li>
+                  <a href="#" target="_new">
+                    Conciertos
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Exposiciones
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Familia
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Teatro
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Deportes
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Cine
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="text-center mt-5 md:mt-0">
+              <p>La empresa</p>
+              <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
+                <li>
+                  <a href="#" target="_new">
+                    Quienes somos
+                  </a>
+                </li>
 
-              <li>
-                <a href="#" target="_new">
-                  Descargar logos
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Términos y condiciones
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Contáctenos
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  TuBoleta Colombia
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="text-center mt-5 md:mt-0">
-            <p>Enlaces de interés</p>
-            <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
-              <li>
-                <a href="#" target="_new">
-                  Botón de arrepentimiento
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Protege TuEntrada
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Familia
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Requisitos de imágenes
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  TuEntrada Wallet
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_new">
-                  Fan a Fan
-                </a>
-              </li>
-            </ul>
+                <li>
+                  <a href="#" target="_new">
+                    Descargar logos
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Términos y condiciones
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Contáctenos
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    TuBoleta Colombia
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="text-center mt-5 md:mt-0">
+              <p>Enlaces de interés</p>
+              <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
+                <li>
+                  <a href="#" target="_new">
+                    Botón de arrepentimiento
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Protege TuEntrada
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Familia
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Requisitos de imágenes
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    TuEntrada Wallet
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_new">
+                    Fan a Fan
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
