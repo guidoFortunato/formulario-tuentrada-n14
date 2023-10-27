0d89637d7303563cec356f62cf97f4eff45d8661
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-10-27 16:38:27 -0300

    cambios

--- src/components/main/Articulo.jsx
+++ src/components/main/Articulo.jsx
@@ -5,11 +5,12 @@ import React from "react";
 
 const Articulo = ({ subcategoria }) => {
   return (
+    <>
     <div className=" container mx-auto bg-main-image bg-no-repeat bg-left-50 pb-10 px-10 md:px-20 flex-1">
       <h2 className="text-2xl text-blue-dark font-semibold mb-10">
         Devoluciones: Recuperar Contraseña
       </h2>
-      <section className="w-full grid grid-cols-1  lg:grid-cols-6 gap-5 lg:gap-3 mx-auto mt-4">
+      <section className="w-full grid grid-cols-1  lg:grid-cols-6 gap-5 lg:gap-3 mx-auto mt-4 mb-10">
         <div className="col-span-4 order-2 lg:order-1">
           <h3
             id="subtitulo1"
@@ -163,37 +164,46 @@ const Articulo = ({ subcategoria }) => {
           </div>
         </div>
       </section>
+<hr />
 
-      <Link href={`${subcategoria}/formulario`} className="flex justify-center">
+
+
+      <div className="flex justify-center items-center flex-col">
+      <h4 className="font-semibold text-center text-blue-dark mt-10 mb-2"> Te sirvió la información?</h4>
+      <Link href={`${subcategoria}/formulario`} >
         <button
           type="button"
-          className="text-white bg-gradient-to-r from-blue-light to-blue-dark hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:blue-dark  font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-10"
+          className="w-auto text-white bg-gradient-to-r from-blue-light to-blue-dark hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:blue-dark  font-medium rounded-md text-sm px-5 py-2.5 text-center  mb-10"
         >
           Completar Formulario
         </button>
       </Link>
+      </div>
+  <hr />
 
-      <div className="flex justify-evenly mt-24">
+ 
+      <div className="flex justify-evenly mt-24 ">
         <div>
-          <h4 className="text-blue-dark font-semibold">Artículos relacionados</h4>
-          <ol>
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
-            <li>Lorem ipsum dolor sit. Lorem ipsum dolor sit.</li>
-            <li>Lorem, ipsum dolor.</li>
-            <li>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</li>
+          <h4 className="text-blue-dark font-semibold mb-2">Artículos relacionados</h4>
+          <ol className="text-sm">
+            <li>- Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+            <li>- Lorem ipsum dolor sit. Lorem ipsum dolor sit.</li>
+            <li>- Lorem, ipsum dolor.</li>
+            <li>- Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</li>
           </ol>
         </div>
         <div>
-        <h4 className="text-blue-dark font-semibold">Artículos más vistos</h4>
-          <ol>
-          <li>Lorem, ipsum. Lorem ipsum dolor sit.</li>
-            <li>Lorem ipsum dolor sit. Lorem ipsum, dolor sit amet consectetur adipisicing.</li>
-            <li>Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit..</li>
-            <li>Lorem ipsum dolor sit amet.lorem2 Lorem, ipsum dolor.</li>
+        <h4 className="text-blue-dark font-semibold mb-2" >Artículos más vistos</h4>
+        <ol className="text-sm">
+          <li>- Lorem, ipsum. Lorem ipsum dolor sit.</li>
+            <li>- Lorem ipsum dolor sit. Lorem ipsum, dolor sit amet consectetur adipisicing.</li>
+            <li>- Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit..</li>
+            <li>- Lorem ipsum dolor sit amet.lorem2 Lorem, ipsum dolor.</li>
           </ol>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
