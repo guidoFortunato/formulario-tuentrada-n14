07d1e7074dd0b04cdd7d598de2b7b79405c091e1
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-11-13 15:03:19 -0300

    autocompletado

--- src/components/header/InputBusqueda.jsx
+++ src/components/header/InputBusqueda.jsx
@@ -1,38 +1,84 @@
 "use client";
 
-import { useState } from "react";
-import { alertaWarning } from "@/helpers/Alertas";
-import { InputSelect } from "./InputSelect";
+import { useMemo, useRef, useState } from "react";
 import { getDataPrueba } from "@/helpers/getInfoTest";
+import { createAutocomplete } from "@algolia/autocomplete-core";
+import Link from "next/link";
 
-const InputBusqueda = () => {
-  const [keyword, setKeyword] = useState("");
-  const [articles, setArticles] = useState([]);
+const AutocompleteItem = ({ data }) => {
+  const { title, slug } = data;
 
-  const handleKeyword = (nombreKeyword) => {
-    if (!nombreKeyword.trim()) {
-      setKeyword("");
-      return;
-    }
-    setKeyword(nombreKeyword);
-  };
+  return (
+    <li>
+      <Link href={`/${slug}`} className="hover:bg-blue-300 flex gap-4 p-4">
+        <div className="flex justify-evenly items-center">
+          <svg
+            className="w-3 h-3 mr-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+          <h3 className="text-sm font-semibold">{title}</h3>
+        </div>
+      </Link>
+    </li>
+  );
+};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (keyword.length === 0) {
-      alertaWarning();
-      return;
-    }
-    if (keyword.length >= 3) {
-      const info = await getDataPrueba(`https://testapi.tuentrada.com/api/v1/atencion-cliente/search-article/${keyword}`);
-      console.log({info})
-      info.data.articles.length > 0 && setArticles(info.data.articles)
-      
-    }
+const InputBusqueda = (props) => {
+  const [autocompleteState, setAutocompleteState] = useState({
+    collections: [],
+    isOpen: false,
+  });
 
-    // handleKeyword("");
-    // console.log("click");
-  };
+  const formRef = useRef(null);
+  const inputRef = useRef(null);
+  const panelRef = useRef(null);
+
+  const autocomplete = useMemo(
+    () =>
+      createAutocomplete({
+        placeholder: "Escribí un texto...",
+        onStateChange: ({ state }) => setAutocompleteState(state),
+        getSources: () => [
+          {
+            sourceId: "categories-api",
+            getItems: async ({ query }) => {
+              if (!!query) {
+                try {
+                  const res = await getDataPrueba(
+                    `https://testapi.tuentrada.com/api/v1/atencion-cliente/search-article/${query}`
+                  );
+                  // console.log(res)
+                  return res.data.articles;
+                } catch (err) {
+                  console.log(err);
+                }
+              }
+            },
+          },
+        ],
+        ...props,
+      }),
+    [props]
+  );
+
+  const formProps = autocomplete.getFormProps({
+    inputElement: inputRef.current,
+  });
+  const inputProps = autocomplete.getInputProps({
+    inputElement: inputRef.current,
+  });
+
   return (
     <>
       <div className="flex justify-center flex-col items-center my-10">
@@ -42,18 +88,15 @@ const InputBusqueda = () => {
           </h1>
         </section>
 
-        <form className="w-[95%] md:w-2/5" onSubmit={handleSubmit}>
+        <form ref={formRef} className="w-[95%] md:w-2/5" {...formProps}>
           <div className="relative">
             <input
-              type="search"
-              id="default-search"
-              className="block w-full p-4 pr-12 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-dark focus:border-blue-dark"
-              placeholder="Buscar categoría, subcategoría..."
-              value={keyword}
-              onChange={(e) => handleKeyword(e.target.value)}
+              ref={inputRef}
+              className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-dark focus:border-blue-dark"
+              {...inputProps}
             />
-            
-            <button
+
+            {/* <button
               type="submit"
               className="absolute top-0 right-0 p-4 text-sm h-full text-white bg-gradient-to-r from-blue-light to-blue-dark hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:blue-dark  font-medium rounded-r-md"
             >
@@ -73,9 +116,32 @@ const InputBusqueda = () => {
                 />
               </svg>
               <span className="sr-only">Search</span>
-            </button>
+            </button> */}
           </div>
-          <InputSelect articles={articles} />
+          {autocompleteState.isOpen && (
+            <div
+              className="absolute bg-gray-100 border-gray-100 z-10 rounded-lg shadow-lg mt-1 overflow-hidden"
+              ref={panelRef}
+              {...autocomplete.getPanelProps()}
+            >
+              {autocompleteState.collections.map((collection, index) => {
+                const { items } = collection;
+
+                return (
+                  <section key={`section-${index}`}>
+                    {items.length > 0 && (
+                      <ul {...autocomplete.getListProps()}>
+                        {items.map((item) => (
+                          <AutocompleteItem key={item.id} data={item} />
+                        ))}
+                      </ul>
+                    )}
+                  </section>
+                );
+              })}
+            </div>
+          )}
+          {/* <InputSelect articles={articles} /> */}
         </form>
       </div>
     </>
