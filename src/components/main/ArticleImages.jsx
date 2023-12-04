6bcd55fe8d619ba2857f267c4f4074af83cf0078
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-12-04 17:24:16 -0300

    cambios artículo

--- src/components/main/ArticleImages.jsx
+++ src/components/main/ArticleImages.jsx
@@ -2,32 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const ArticleImages = ({ itemColumn }) => {
- 
   return (
     <>
-    <div className="flex justify-evenly">
-    {itemColumn.link ? (
-        <Link  href={itemColumn.link} target={itemColumn.target}>
+      <div className="flex justify-evenly relative">
+        {itemColumn.link ? (
+          <Link href={itemColumn.link} target={itemColumn.target}>
+            <Image
+              key={itemColumn.image?.src}
+              src={itemColumn.image?.src}
+              alt={itemColumn.alt}
+              width={700}
+              height={700}
+              className="rounded-lg"
+              style={{ width: "auto", height: "auto",}}
+
+            />
+          </Link>
+        ) : (
           <Image
             key={itemColumn.image?.src}
             src={itemColumn.image?.src}
             alt={itemColumn.alt}
-            width={300}
-            height={300}
+           
+            width={700}
+            height={700}
             className="rounded-lg"
+            style={{ width: "auto", height: "auto" }}
+
           />
-        </Link>
-      ) : (
-        <Image 
-          key={itemColumn.image?.src}
-          src={itemColumn.image?.src}
-          alt={itemColumn.alt}
-          width={300}
-          height={300}
-          className="rounded-lg "
-        />
-      )}
-    </div>
+        )}
+      </div>
     </>
   );
 };
--- src/components/main/ArticleRows.jsx
+++ src/components/main/ArticleRows.jsx
@@ -8,7 +8,9 @@ export const ArticleRows = ({item}) => {
   const columns = item.columns
   // console.log({columns})
   return (
-    <section className={`grid ${item.width === "100%" && "grid-cols-1"} ${item.width === "50%-50%" && "grid-cols-1 md:grid-cols-2 gap-4"} ${item.width === "33%-33%-33%" && "grid-cols-1 lg:grid-cols-3 mb-3"} ${item.width === "25%-25%-25%-25%" && "grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"} ${item.width === "25%-75%" && "grid-cols-1 lg:grid-cols-3 gap-4 mb-3"} mb-3`}>
+
+<section className={`grid ${item.width === "100%" && "grid-cols-1"} ${item.width === "50%-50%" && "grid-cols-1 md:grid-cols-2 gap-4"} ${item.width === "33%-33%-33%" && "grid-cols-1 lg:grid-cols-3 gap-6"} ${item.width === "25%-25%-25%-25%" && "grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"} ${item.width === "25%-75%" && "grid-cols-1 lg:grid-cols-3 lg:gap-6 gap-0"} mb-3`}>
+
       {
         columns.map( column => {
           // console.log({column})
@@ -20,12 +22,12 @@ export const ArticleRows = ({item}) => {
             if (itemColumn.type === 'imagenes') {
               return <ArticleImages key={itemColumn.order_column} itemColumn={itemColumn} />
             }
-            // if (itemColumn.type === 'acordion') {
-            //   return <ArticleAccordion key={itemColumn.order_column} itemColumn={itemColumn} />
-            // }
+            //  if (itemColumn.type === 'acordion') {
+            //     return <ArticleAccordion key={itemColumn.order_column} itemColumn={itemColumn} />
+            //    }
             // if (itemColumn.type === 'botones') {
-            //   return <ArticleButtons   key={itemColumn.order_column} itemColumn={itemColumn} />
-            // }
+            //    return <ArticleButtons   key={itemColumn.order_column} itemColumn={itemColumn} />
+            //   }
 
           } )
         } )
